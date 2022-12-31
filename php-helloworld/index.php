f5fb5a7d281b449a32d355196db8f99e83c31fa7
Student User <student@workstation.lab.example.com>
2022-12-31 13:22:15 -0500

    praveen changed

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,4 @@
 <?php
-print "Hello, World! php version is updated " . PHP_VERSION . "\n";
+print "Hello, World! php version is updated -new branch (s2i) " . PHP_VERSION . "\n";
 print "A change is a coming!\n";
 ?>
